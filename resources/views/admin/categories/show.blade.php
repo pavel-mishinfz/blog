2244eac41862ac937a50d722dedd6ab281432ce7
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-13 17:29:14 +0300

    Added delete and edit actions

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -5,8 +5,19 @@
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">{{ $category->title }}</h1>
+                    <div class="col-sm-6 d-flex align-items-center">
+                        <h1 class="m-0 mr-3">{{ $category->title }}</h1>
+                        <a href="{{ route('admin.category.edit', $category->id) }}" class="text-success mr-2">
+                            <i class="fa fa-pencil-alt"></i>
+                        </a>
+                        <form action="{{ route('admin.category.delete', $category->id) }}"
+                              method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="border-0 bg-transparent">
+                                <i class="fa fa-trash text-danger"></i>
+                            </button>
+                        </form>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
