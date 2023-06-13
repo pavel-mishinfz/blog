20cf2fd85d57f5b75bc9bfd110659b9fc4b11d35
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-13 21:54:14 +0300

    Change path to view

--- app/Http/Controllers/Admin/Category/CreateController.php
+++ app/Http/Controllers/Admin/Category/CreateController.php
@@ -8,6 +8,6 @@ class CreateController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.categories.create');
+        return view('admin.category.create');
     }
 }
