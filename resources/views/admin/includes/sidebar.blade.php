69fa1fd7d110e86fc84f54db1992b7c3fee7df27
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-13 10:41:11 +0300

    Created sidebar blade

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,22 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="{{ route('admin.category.index') }}" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>
+                            Категории
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!-- /.sidebar -->
+</aside>
