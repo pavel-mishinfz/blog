8768b42c5118c3e1174476502f7fc260e987a229
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-16 10:41:06 +0300

    Fix an array with tag IDs

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -14,13 +14,17 @@ class PostService
     {
         try {
             DB::beginTransaction();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
 
             $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
             $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
             $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
+            if(isset($tagIds)) {
+                $post->tags()->attach($tagIds);
+            }
             DB::commit();
         } catch (Exception $exception) {
             DB::rollBack();
@@ -28,11 +32,14 @@ class PostService
         }
     }
 
-    public function update($data, $post) {
+    public function update($data, $post)
+    {
         try {
             DB::beginTransaction();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
 
             isset($data['preview_image']) ? $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']) : '';
             isset($data['main_image']) ? $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']) : '';
