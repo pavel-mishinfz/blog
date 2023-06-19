b91f3ad8e9bea9bfdf7b4044b272b37c255d79e7
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-19 11:39:37 +0300

    Added the output of comments

--- resources/views/personal/comment/index.blade.php
+++ resources/views/personal/comment/index.blade.php
@@ -24,7 +24,47 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-
+                    <div class="col-6">
+                        <div class="card">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Сообщение</th>
+                                        <th colspan="2" class="text-center">Действия</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($comments as $comment)
+                                        <tr>
+                                            <td>{{ $comment->id }}</td>
+                                            <td>{{ $comment->message }}</td>
+                                            <td class="text-center">
+                                                <a href="{{ route('personal.comment.edit', $comment->id) }}"
+                                                   class="text-success">
+                                                    <i class="fa fa-pencil-alt"></i>
+                                                </a>
+                                            </td>
+                                            <td class="text-center">
+                                                <form action="{{ route('personal.comment.delete', $comment->id) }}"
+                                                      method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="border-0 bg-transparent">
+                                                        <i class="fa fa-trash text-danger"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
                 </div>
                 <!-- /.row -->
             </div><!-- /.container-fluid -->
