d0aad9e766f5b7b7c93df27ed2e77fa360d17e84
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-19 10:07:08 +0300

    Created sidebar for the user's personal account

---
+++ resources/views/personal/includes/sidebar.blade.php
@@ -0,0 +1,38 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="{{ route('personal.main.index') }}" class="nav-link">
+                        <i class="nav-icon fa fa-home"></i>
+                        <p>
+                            Главная
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('personal.liked.index') }}" class="nav-link">
+                        <i class="nav-icon fa fa-heart"></i>
+                        <p>
+                            Понравившиеся посты
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('personal.comment.index') }}" class="nav-link">
+                        <i class="nav-icon fa fa-comment"></i>
+                        <p>
+                            Комментарии
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <!-- /.sidebar -->
+</aside>
