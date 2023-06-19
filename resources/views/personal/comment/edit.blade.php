95dcd8e6c1d6870e9b8962b1ea7f80664b22d4f8
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-06-19 11:38:00 +0300

    Created a template for edit comments

---
+++ resources/views/personal/comment/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('personal.layouts.main')
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Редактирование комментария</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{ route('personal.comment.update', $comment->id) }}" method="post" class="w-50">
+                            @csrf
+                            @method('PATCH')
+                            <div class="form-group">
+                                <label>Сообщение</label>
+                                <textarea class="form-control" name="message" cols="30" rows="10">{{ $comment->message }}</textarea>
+                                @error('message')
+                                <p class="text-danger">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Обновить">
+                        </form>
+                    </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+@endsection
